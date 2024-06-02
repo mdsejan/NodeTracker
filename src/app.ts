e7c072a63e60c5abe68fec87ede8fe843033ef1d
Sejan~ <md4sejan@gmail.com>
2024-06-03 00:58:43 +0600

    globalerrhandler , not found

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import { StudentRoutes } from './app/modules/student/student.route';
 import { UserRoutes } from './app/modules/user/user.route';
+import globalErrorHandler from './app/middlewares/globalErrorhandler';
+import notFound from './app/middlewares/notFound';
 const app: Application = express();
 
 //parsers
@@ -16,5 +18,9 @@ app.use('/api/v1/users', UserRoutes);
 app.get('/', (req: Request, res: Response) => {
   res.send('Server Running');
 });
+
+app.use(globalErrorHandler);
+
+app.use(notFound);
 
 export default app;
